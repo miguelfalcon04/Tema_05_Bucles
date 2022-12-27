c392edd88c1c0e4ed5fbe51fb8dd77897e23d1e6
miguelfal <falconmiguel.2004@gmail.com>
2022-12-27 14:15:47 +0100

    commit

--- Eje08.java
+++ Eje08.java
@@ -1,5 +1,21 @@
+/* Ejercicio 8
+Muestra la tabla de multiplicar de un número introducido por teclado.
+
+@author miguelfalcon
+*/
+
+import java.util.Scanner;
 public class Eje08 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Se le mostrará la tabla del: ");
+        int tabla = sc.nextInt();
+
+        for(int i =1; i<=10; i++){
+            System.out.printf("%2d x %d = %2d\n",i,tabla,(i*tabla));
+        }
+
+        sc.close();
     }
 }
